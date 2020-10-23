54a2d3f23c059f0e154ed98e8c461afaa16434c9
Veljko Stanojevic <stveljko012@gmail.com>
2020-10-23 18:28:38 +0200

    build

---
+++ build/precache-manifest.2036b7b7c37699b74a92c49325e8014b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bcd98b0913dfbfd756901865bf898bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3eeb999a39ca965165d6",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "92cbb8e60f53d04db70e",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "3eeb999a39ca965165d6",
+    "url": "/static/js/2.76286cf9.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/static/js/2.76286cf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92cbb8e60f53d04db70e",
+    "url": "/static/js/main.e0695657.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
