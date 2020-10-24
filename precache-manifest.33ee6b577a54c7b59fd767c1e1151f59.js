7799ed85c07156965dc5955aeb82c63c404f4ba5
Veljko Stanojevic <stveljko012@gmail.com>
2020-10-24 12:57:17 +0200

    Updates

---
+++ precache-manifest.33ee6b577a54c7b59fd767c1e1151f59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcaf713a8138cbeef88cc7b2b5512f36",
+    "url": "/stveljko012/banjex-phonebook/index.html"
+  },
+  {
+    "revision": "3eeb999a39ca965165d6",
+    "url": "/stveljko012/banjex-phonebook/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "d35310d018b4a8b62597",
+    "url": "/stveljko012/banjex-phonebook/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "3eeb999a39ca965165d6",
+    "url": "/stveljko012/banjex-phonebook/static/js/2.76286cf9.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/stveljko012/banjex-phonebook/static/js/2.76286cf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d35310d018b4a8b62597",
+    "url": "/stveljko012/banjex-phonebook/static/js/main.72ceb28d.chunk.js"
+  },
+  {
+    "revision": "54d2525985f47e57fcae",
+    "url": "/stveljko012/banjex-phonebook/static/js/runtime-main.2c480b6e.js"
+  }
+]);
